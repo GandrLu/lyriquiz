b3be617664722b5ea8a68cf990b3800c71f43d18
acidyumyum <acidyumyum@web.de>
2019-01-16 18:37:09 +0100

    Update environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDnw27OArUyY3xLzep9tjcCXhadvDvnCGk",
+    authDomain: "lyriquiz-c2b3f.firebaseapp.com",
+    databaseURL: "https://lyriquiz-c2b3f.firebaseio.com",
+    projectId: "lyriquiz-c2b3f",
+    storageBucket: "lyriquiz-c2b3f.appspot.com",
+    messagingSenderId: "772009897207"
+  }
 };
